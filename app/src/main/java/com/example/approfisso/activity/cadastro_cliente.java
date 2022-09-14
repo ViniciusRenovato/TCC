a0bfd07eb33881194421e0b9a2525899a0d82054
MatheusCampus <83256336+MatheusCampus@users.noreply.github.com>
2022-09-14 18:41:10 -0400

    Data no cliente cadastro
    configurado a inserção correta da data no cadastro no cliente para ser usado como base nas outras entidades

--- app/src/main/java/com/example/approfisso/activity/cadastro_cliente.java
+++ app/src/main/java/com/example/approfisso/activity/cadastro_cliente.java
@@ -1,8 +1,11 @@
 package com.example.approfisso.activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 
@@ -13,6 +16,10 @@ import com.google.firebase.database.DatabaseReference;
 
 import com.example.approfisso.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 
 public class cadastro_cliente extends AppCompatActivity {
 
@@ -21,7 +28,23 @@ public class cadastro_cliente extends AppCompatActivity {
     private EditText nome;
     private EditText sobrenome;
     private EditText telefone;
+
     private EditText aniversario;
+    Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+
+
+
+
+
     private EditText email;
     private Pessoa Clientes;
 
@@ -32,6 +55,11 @@ public class cadastro_cliente extends AppCompatActivity {
         setContentView(R.layout.cliente_cadastro);
 
 
+
+
+
+
+
         nome=findViewById(R.id.Nome_Servico);
         sobrenome=findViewById(R.id.Duracao_Servico);
         telefone=findViewById(R.id.Preco_Servico);
@@ -39,6 +67,14 @@ public class cadastro_cliente extends AppCompatActivity {
         email=findViewById(R.id.Email_cadastro);
         Intent i = getIntent();
         Clientes =(Pessoa) i.getSerializableExtra("Clientes");
+
+        aniversario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(cadastro_cliente.this, date, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
 
     }
@@ -59,6 +95,13 @@ public class cadastro_cliente extends AppCompatActivity {
         finish();
     }
 
+    private void updateLabel(){
+
+        String myFormat = "dd/MM/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, new Locale("pt","BR"));
+
+        aniversario.setText(sdf.format(myCalendar.getTime()));
+    }
 
     public void botao_Confirmar (View view){
         Clientes = new Pessoa();
