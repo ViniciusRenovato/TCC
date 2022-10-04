a6eaf7b3c63e64b3a7768ae11c09e99cac69d2f2
MatheusCampus <83256336+MatheusCampus@users.noreply.github.com>
2022-10-04 18:34:05 -0400

    Firestore
    Nome resgatado do firestore

--- app/src/main/java/com/example/approfisso/activity/Principal.java
+++ app/src/main/java/com/example/approfisso/activity/Principal.java
@@ -2,11 +2,14 @@ package com.example.approfisso.activity;
 
 import static com.example.approfisso.activity.LoginActivity.Username;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.approfisso.R;
 import com.google.android.gms.common.util.SharedPreferencesUtils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -16,6 +19,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -55,6 +59,25 @@ public class Principal extends AppCompatActivity {
         super.onStart();
 
         FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final String current = user.getUid();
+
+        db.collection("usuários")
+                .whereEqualTo("id",current).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for(DocumentSnapshot document : task.getResult()){
+                                mTVEmail.setText((CharSequence) document.get("nome")) ;
+                            }
+                        }
+                    }
+                });
+
+
+
 
 
 
