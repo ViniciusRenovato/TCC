9e9b21f45db0b0ebb786f8ddfb3e25db2989b662
MatheusCampus <83256336+MatheusCampus@users.noreply.github.com>
2022-10-08 11:17:16 -0400

    Botão de Logout arrumado
    Quando sai do aplicativo após deslogar e entra novamente o shared preferences não fica mais null, o que acabava quebrando o app

--- app/src/main/java/com/example/approfisso/activity/Principal.java
+++ app/src/main/java/com/example/approfisso/activity/Principal.java
@@ -13,7 +13,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -106,7 +109,12 @@ public class Principal extends AppCompatActivity {
     }
 
     public void logout(View view){
+        SharedPreferences preferences = getSharedPreferences("login", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.apply();
         mFirebaseAuth.signOut();
+
         onBackPressed();
     }
 
